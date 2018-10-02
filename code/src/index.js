0d1d49a805c467a697bb4f4870ff831f4e757a8d
Jeanette <aejlindberg@gmail.com>
2018-10-02 11:09:17 +0200

    Ticking clock

--- code/src/index.js
+++ code/src/index.js
@@ -4,15 +4,42 @@ import "./index.css"
 
 class Clock extends React.Component {
 
+    state = {
+      date: new Date()
+    }
+
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      )
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.timerID)
+    }
+
+    tick() {
+      this.setState({
+        date: new Date()
+      })
+    }
+
+  
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button>Click me</button>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(
+  <Clock date/>,
+  document.getElementById("root")
+)
